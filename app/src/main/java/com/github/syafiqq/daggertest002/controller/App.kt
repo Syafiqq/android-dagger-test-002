678a6bd2577d44c79999c8909b0f76f618b6b5f4
Syafiq <syafiq.rezpector@gmail.com>
2019-09-23 18:44:03 +0700

    change to dagger component

--- app/src/main/java/com/github/syafiqq/daggertest002/controller/App.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/controller/App.kt
@@ -1,11 +1,17 @@
 package com.github.syafiqq.daggertest002.controller
 
-import android.app.Application
 import android.util.Log
 import com.github.syafiqq.daggertest002.BuildConfig
+import com.github.syafiqq.daggertest002.model.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
 import timber.log.Timber
 
-class App : Application() {
+class App : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.factory().create(this)
+    }
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
