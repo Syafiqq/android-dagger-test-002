21af683fd98fdcf2f4e67f9f2823ebfc8d3ed184
Syafiq <syafiq.rezpector@gmail.com>
2019-09-23 19:24:37 +0700

    remove singleton

--- app/src/main/java/com/github/syafiqq/daggertest002/mode/concurrent/SchedulerProviderImpl.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/mode/concurrent/SchedulerProviderImpl.kt
@@ -4,9 +4,7 @@ import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class SchedulerProviderImpl @Inject constructor() : SchedulerProvider {
     override fun computation(): Scheduler {
         return Schedulers.computation()
