ee18b6cf2f8e2711c7381e3e9f3f722b17beebcd
Syafiq <syafiq.rezpector@gmail.com>
2019-09-23 21:14:25 +0700

    inject test

--- app/src/main/java/com/github/syafiqq/daggertest002/controller/App.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/controller/App.kt
@@ -1,13 +1,23 @@
 package com.github.syafiqq.daggertest002.controller
 
+import android.content.Context
+import android.os.Handler
 import android.util.Log
+import androidx.core.os.postDelayed
 import com.github.syafiqq.daggertest002.BuildConfig
+import com.github.syafiqq.daggertest002.mode.concurrent.SchedulerProvider
 import com.github.syafiqq.daggertest002.model.di.component.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import timber.log.Timber
+import javax.inject.Inject
 
 class App : DaggerApplication() {
+    @Inject
+    lateinit var context:Context
+    @Inject
+    lateinit var schedulers:SchedulerProvider
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory().create(this)
     }
@@ -18,6 +28,11 @@ class App : DaggerApplication() {
             Timber.plant(Timber.DebugTree())
         } else {
             Timber.plant(CrashReportingTree())
+        }
+
+        Handler().postDelayed(2000) {
+            Timber.d("Context : ${context == null}")
+            Timber.d("SchedulerProvider : ${schedulers == null}")
         }
     }
 
