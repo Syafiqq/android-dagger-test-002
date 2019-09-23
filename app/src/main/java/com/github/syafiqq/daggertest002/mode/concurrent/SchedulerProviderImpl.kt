f3559422fc6e4c32eefaf2fc19061296bf521007
Syafiq <syafiq.rezpector@gmail.com>
2019-09-23 19:12:15 +0700

    mark as singleton

--- app/src/main/java/com/github/syafiqq/daggertest002/mode/concurrent/SchedulerProviderImpl.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/mode/concurrent/SchedulerProviderImpl.kt
@@ -3,8 +3,11 @@ package com.github.syafiqq.daggertest002.mode.concurrent
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class SchedulerProviderImpl : SchedulerProvider {
+@Singleton
+class SchedulerProviderImpl @Inject constructor() : SchedulerProvider {
     override fun computation(): Scheduler {
         return Schedulers.computation()
     }
