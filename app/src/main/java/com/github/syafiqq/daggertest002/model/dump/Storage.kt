904d77222b55ec51e52783fc7ae7ab478ef04531
Syafiq <syafiq.rezpector@gmail.com>
2019-09-24 12:18:07 +0700

    fix user storage

--- app/src/main/java/com/github/syafiqq/daggertest002/model/dump/Storage.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/model/dump/Storage.kt
@@ -6,10 +6,10 @@ import com.github.syafiqq.daggertest002.model.entity.UserEntityImpl
 object Storage {
     val users: Map<String, UserEntity> by lazy {
         mapOf(
-            "1" to UserEntityImpl("1", "name-1", "mail1@mail.com", "pass1"),
-            "2" to UserEntityImpl("2", "name-2", "mail2@mail.com", "pass2"),
-            "3" to UserEntityImpl("3", "name-3", "mail3@mail.com", "pass3"),
-            "4" to UserEntityImpl("4", "name-4", "mail4@mail.com", "pass4")
+            "1" to UserEntityImpl("1", "name1", "mail1@mail.com", "pass1"),
+            "2" to UserEntityImpl("2", "name2", "mail2@mail.com", "pass2"),
+            "3" to UserEntityImpl("3", "name3", "mail3@mail.com", "pass3"),
+            "4" to UserEntityImpl("4", "name4", "mail4@mail.com", "pass4")
         )
     }
 }
