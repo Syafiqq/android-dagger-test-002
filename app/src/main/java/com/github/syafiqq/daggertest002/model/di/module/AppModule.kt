edcc15cf2c0d4ace2e9d147eb138423603bc6f01
Syafiq <syafiq.rezpector@gmail.com>
2019-09-24 20:15:09 +0700

    register app injector

--- app/src/main/java/com/github/syafiqq/daggertest002/model/di/module/AppModule.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/model/di/module/AppModule.kt
@@ -6,6 +6,8 @@ import com.github.syafiqq.daggertest002.model.api.IdentityServer
 import com.github.syafiqq.daggertest002.model.api.IdentityServerImpl
 import com.github.syafiqq.daggertest002.model.concurrent.SchedulerProvider
 import com.github.syafiqq.daggertest002.model.concurrent.SchedulerProviderImpl
+import com.github.syafiqq.daggertest002.model.di.misc.AppInjectorContract
+import com.github.syafiqq.daggertest002.model.di.misc.AppInjectorImpl
 import com.github.syafiqq.daggertest002.model.dump.CounterContract
 import com.github.syafiqq.daggertest002.model.dump.CounterImpl
 import com.github.syafiqq.daggertest002.model.service.identity.UserManager
@@ -14,7 +16,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
-import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module(includes = [StaticAppModule::class])
@@ -29,6 +30,11 @@ object StaticAppModule {
     @Singleton
     @JvmStatic
     fun provideApplicationContext(app: App): Context = app
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun provideAppInjector(app: App): AppInjectorContract = AppInjectorImpl(app)
 
     @Provides
     @Singleton
