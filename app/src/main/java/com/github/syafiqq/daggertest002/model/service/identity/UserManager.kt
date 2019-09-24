4b80ee849cfd553622fc1f04c5e5b579daf60e08
Syafiq <syafiq.rezpector@gmail.com>
2019-09-24 10:27:33 +0700

    add user manager impl

---
+++ app/src/main/java/com/github/syafiqq/daggertest002/model/service/identity/UserManager.kt
@@ -0,0 +1,10 @@
+package com.github.syafiqq.daggertest002.model.service.identity
+
+import com.github.syafiqq.daggertest002.model.entity.UserEntity
+import io.reactivex.Flowable
+
+interface UserManager {
+    fun login(email: String, password: String): Flowable<UserEntity>
+    fun logout(): Flowable<UserEntity>
+    fun getUser(id: String): Flowable<UserEntity>
+}
