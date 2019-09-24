191a33b1836035d567119f11cb5448838af27037
Syafiq <syafiq.rezpector@gmail.com>
2019-09-24 12:21:18 +0700

    Fixing login activity

--- app/src/main/java/com/github/syafiqq/daggertest002/controller/LoginActivity.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/controller/LoginActivity.kt
@@ -1,18 +1,33 @@
 package com.github.syafiqq.daggertest002.controller
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.UserManager
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.postDelayed
 import com.github.syafiqq.daggertest002.R
+import com.github.syafiqq.daggertest002.model.concurrent.SchedulerProvider
 import com.github.syafiqq.daggertest002.model.dump.CounterContract
+import com.github.syafiqq.daggertest002.model.entity.UserEntity
 import dagger.android.AndroidInjection
+import io.reactivex.Observer
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.activity_login.*
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Named
 
 class LoginActivity : AppCompatActivity() {
+    @Inject
+    lateinit var manager: com.github.syafiqq.daggertest002.model.service.identity.UserManager
+    @Inject
+    lateinit var context: Context
+    @Inject
+    lateinit var schedulers: SchedulerProvider
     @Inject
     @field:Named("app-scope")
     lateinit var counter: CounterContract
@@ -38,5 +53,30 @@ class LoginActivity : AppCompatActivity() {
                 Timber.d("Activity Counter : [${counter1.value}]")
             }
         }
+
+        val s: Observer<View> = PublishSubject.create<View>().apply {
+            throttleFirst(350, TimeUnit.MILLISECONDS)
+            subscribe(::onLogin)
+        }
+
+        button.setOnClickListener(s::onNext)
+    }
+
+    private fun onLogin(view: View) {
+        fun loginSuccess(user: UserEntity) {
+            Toast.makeText(context, "Welcome ${user.name}", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, HomeActivity::class.java)).also {
+                finish()
+            }
+        }
+        fun loginFailed(error: Throwable) {
+            Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+        }
+
+        manager
+            .login(email.editText?.text.toString(), password.editText?.text.toString())
+            .subscribeOn(schedulers.computation())
+            .observeOn(schedulers.ui())
+            .subscribe(::loginSuccess, ::loginFailed)
     }
 }
