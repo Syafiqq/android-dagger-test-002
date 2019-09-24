30301c8bb941685033a3e7ce08c097a4961a9b43
Syafiq <syafiq.rezpector@gmail.com>
2019-09-24 13:26:46 +0700

    add user component

---
+++ app/src/main/java/com/github/syafiqq/daggertest002/model/di/component/UserComponent.kt
@@ -0,0 +1,12 @@
+package com.github.syafiqq.daggertest002.model.di.component
+
+import com.github.syafiqq.daggertest002.model.di.module.UserMapperModule
+import com.github.syafiqq.daggertest002.model.di.scope.UserScope
+import dagger.Component
+
+@UserScope
+@Component(
+    modules = [UserMapperModule::class],
+    dependencies = [AppComponent::class]
+)
+interface UserComponent
