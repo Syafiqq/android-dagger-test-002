0ae07222adc6c9974bf64123fc0b329c98ada78c
Syafiq <syafiq.rezpector@gmail.com>
2019-09-25 18:20:26 +0700

    add home activity

--- app/src/main/java/com/github/syafiqq/daggertest002/controller/HomeActivity.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/controller/HomeActivity.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.postDelayed
 import com.github.syafiqq.daggertest002.R
 import com.github.syafiqq.daggertest002.custom.dagger.android.AndroidInjection
+import com.github.syafiqq.daggertest002.custom.dagger.android.support.DaggerAppCompatActivity
 import com.github.syafiqq.daggertest002.model.di.component.UserComponent
 import io.reactivex.Observer
 import io.reactivex.subjects.PublishSubject
@@ -16,9 +16,11 @@ import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class HomeActivity : AppCompatActivity(), DetailFragment.InteractionListener {
+class HomeActivity : DaggerAppCompatActivity(), DetailFragment.InteractionListener {
     @Inject
     lateinit var manager: com.github.syafiqq.daggertest002.model.service.identity.UserManager
+
+    override fun desiredComponent(): Class<*> = UserComponent::class.java
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this, UserComponent::class.java)
