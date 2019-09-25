375532714d1b948ec8b45505c9530f2e577dcdcf
Syafiq <syafiq.rezpector@gmail.com>
2019-09-25 18:01:36 +0700

    Add user detail

---
+++ app/src/main/java/com/github/syafiqq/daggertest002/controller/UserDetailActivity.kt
@@ -0,0 +1,24 @@
+package com.github.syafiqq.daggertest002.controller
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.github.syafiqq.daggertest002.R
+
+import kotlinx.android.synthetic.main.activity_user_detail.*
+
+class UserDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
