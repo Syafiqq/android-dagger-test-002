c4ce65fbb34e341cb37f0615c227a1275a8525f4
Syafiq <syafiq.rezpector@gmail.com>
2019-09-25 17:52:53 +0700

    add activity scope

--- app/src/main/java/com/github/syafiqq/daggertest002/model/di/module/UserMapperModule.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/model/di/module/UserMapperModule.kt
@@ -1,6 +1,7 @@
 package com.github.syafiqq.daggertest002.model.di.module
 
 import com.github.syafiqq.daggertest002.controller.HomeActivity
+import com.github.syafiqq.daggertest002.model.di.scope.ActivityScope
 import com.github.syafiqq.daggertest002.model.di.scope.UserScope
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -8,5 +9,6 @@ import dagger.android.ContributesAndroidInjector
 @Module
 abstract class UserMapperModule {
     @ContributesAndroidInjector
+    @ActivityScope
     abstract fun contributeHomeActivityInjector(): HomeActivity
 }
