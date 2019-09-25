3b852e751272f0b88ca9b4b5ca2f54dc854eb2da
Syafiq <syafiq.rezpector@gmail.com>
2019-09-25 18:03:22 +0700

    register user detail

--- app/src/main/java/com/github/syafiqq/daggertest002/model/di/module/UserMapperModule.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/model/di/module/UserMapperModule.kt
@@ -1,8 +1,8 @@
 package com.github.syafiqq.daggertest002.model.di.module
 
 import com.github.syafiqq.daggertest002.controller.HomeActivity
+import com.github.syafiqq.daggertest002.controller.UserDetailActivity
 import com.github.syafiqq.daggertest002.model.di.scope.ActivityScope
-import com.github.syafiqq.daggertest002.model.di.scope.UserScope
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -11,4 +11,8 @@ abstract class UserMapperModule {
     @ContributesAndroidInjector
     @ActivityScope
     abstract fun contributeHomeActivityInjector(): HomeActivity
+
+    @ContributesAndroidInjector
+    @ActivityScope
+    abstract fun contributeDetailActivityInjector(): UserDetailActivity
 }
