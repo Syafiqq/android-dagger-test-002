bc9e6c9f581d6c2d78b85c567a5809043a3dd1da
Syafiq <syafiq.rezpector@gmail.com>
2019-09-25 18:07:24 +0700

    change fab

--- app/src/main/java/com/github/syafiqq/daggertest002/controller/HomeActivity.kt
+++ app/src/main/java/com/github/syafiqq/daggertest002/controller/HomeActivity.kt
@@ -1,15 +1,19 @@
 package com.github.syafiqq.daggertest002.controller
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.postDelayed
 import com.github.syafiqq.daggertest002.R
 import com.github.syafiqq.daggertest002.custom.dagger.android.AndroidInjection
 import com.github.syafiqq.daggertest002.model.di.component.UserComponent
-import com.google.android.material.snackbar.Snackbar
+import io.reactivex.Observer
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_home.*
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity(), DetailFragment.InteractionListener {
@@ -22,10 +26,15 @@ class HomeActivity : AppCompatActivity(), DetailFragment.InteractionListener {
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val s: Observer<View> = PublishSubject.create<View>().apply {
+            throttleFirst(350, TimeUnit.MILLISECONDS)
+            subscribe {
+                startActivity(Intent(this@HomeActivity, UserDetailActivity::class.java))
+            }
         }
+
+        fab.setOnClickListener(s::onNext)
+
         Handler().postDelayed(100) {
             Timber.d("User Manager ${manager == null}")
         }
